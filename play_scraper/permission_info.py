61cf59ece93731395619c1d7ab67b074e7952afb
grtushar <md.tushar_91@yahoo.com>
2020-06-25 01:05:53 +0600

    :sparkles: add retry untill get permission data option

--- play_scraper/permission_info.py
+++ play_scraper/permission_info.py
@@ -13,32 +13,35 @@ driver = webdriver.Chrome(chrome_driver_path, options=options)
 def get_permission_info(url):
     ret = ""
 
-    driver.get(url)
-    time.sleep(5)
-    source = driver.page_source
-    soup1 = BeautifulSoup(source, "lxml")
-    view_details_link = soup1.find('a', {'class': 'hrTbp', 'jsname': 'Hly47e'})
-    view_details_link = unidecode(view_details_link.text)
-    driver.find_element_by_link_text(view_details_link).click()
-    time.sleep(5)
-    source2 = driver.page_source
-    soup2 = BeautifulSoup(source2, "lxml")
+    while True:
+        driver.get(url)
+        time.sleep(5)
+        source = driver.page_source
+        soup1 = BeautifulSoup(source, "lxml")
+        view_details_link = soup1.find('a', {'class': 'hrTbp', 'jsname': 'Hly47e'})
+        view_details_link = unidecode(view_details_link.text)
+        driver.find_element_by_link_text(view_details_link).click()
+        time.sleep(5)
+        source2 = driver.page_source
+        soup2 = BeautifulSoup(source2, "lxml")
 
-    permission_sub_lists = soup2.find_all('ul', {'class': 'GLaCt'})
-    permission_contents = []
-    for contents in permission_sub_lists:
-        plain_contents = []
-        for data in contents.find_all('li'):
-            plain_contents.append(data.text)
-        permission_contents.append(plain_contents)
+        permission_sub_lists = soup2.find_all('ul', {'class': 'GLaCt'})
+        permission_contents = []
+        for contents in permission_sub_lists:
+            plain_contents = []
+            for data in contents.find_all('li'):
+                plain_contents.append(data.text)
+            permission_contents.append(plain_contents)
 
-    permission_list = soup2.find_all('span', {'class': 'SoU6Qc'})
-    for i in range(0, len(permission_list)):
-        permission = permission_list[i]
-        ret += permission.text + "\n"
-        print(permission.text)
-        for sublistContent in permission_contents[i]:
-            ret += sublistContent + "\n"
-            print("-" + sublistContent)
+        permission_list = soup2.find_all('span', {'class': 'SoU6Qc'})
+        for i in range(0, len(permission_list)):
+            permission = permission_list[i]
+            ret += permission.text + "\n"
+            print(permission.text)
+            for sublistContent in permission_contents[i]:
+                ret += sublistContent + "\n"
+                print("-" + sublistContent)
+        if len(permission_list) > 0 and len(permission_sub_lists):
+            break
 
     return ret
